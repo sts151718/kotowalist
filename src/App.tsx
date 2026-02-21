99f224ba7db0329baf582c4f3d059b4967f7a055
sts151718 <tsubokurajob151718@gmail.com>
2026-02-21 14:53:47 +0900

    vitest導入・動作確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@chakra-ui/react';
+import { Button, Heading } from '@chakra-ui/react';
 
 function App() {
   return (
-    <>
+    <div>
+      <Heading as="h1">テストタイトル</Heading>
       <Button colorPalette="teal">ボタン</Button>
-    </>
+    </div>
   );
 }
 
